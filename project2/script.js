5d3e356676fe92947421a277d04d5f0cce13da5f
Richard Škoda <richard.skoda72@gmail.com>
2022-07-25 20:56:58 +0200

    Rework project2(todo app).

--- project2/script.js
+++ project2/script.js
@@ -1,29 +1,63 @@
 let myToDos = [{
-  text: 'Wash the car',
+  task: 'Wash the car',
   completion: 'yes'
 }, {
-  text: 'Tide up the house',
+  task: 'Tide up the house',
   completion: 'no'
 }, {
-  text: 'Take out rubbish',
+  task: 'Take out rubbish',
   completion: 'no'
 }, {
-  text: 'Feed the dog',
+  task: 'Feed the dog',
   completion: 'yes'
 }, {
-  text: 'Fedd the cat',
+  task: 'Fedd the cat',
   completion: 'no'
 }];
 
-let toDoLeft = myToDos.filter(function (todo) {
-  return todo.completion === 'no';
+
+const searchInput = document.querySelector('.search-bar');
+const searchedList = document.querySelector('main');
+
+function renderSearchedTodos(todos, searchingText) {
+  searchedList.innerHTML = '';
+
+  let results = todos.filter(function (todo) {
+    return todo.task.toLowerCase().includes(searchingText.toLowerCase());
+  });
+  results.forEach(function (result) {
+    for (key in result) {
+      const oneTodo = document.createElement('p')
+      oneTodo.textContent = `${key}: ${result[key]}`
+      oneTodo.style = 'margin-top: 0px; margin-bottom: 0px'
+      searchedList.appendChild(oneTodo)
+    };
+    const blankLine = document.createElement('br');
+    searchedList.appendChild(blankLine);
+  });
+};
+
+searchInput.addEventListener('input', function (event) {
+  searchingText = event.target.value;
+  renderSearchedTodos(myToDos, searchingText);
 });
 
+
+/*
 for(let i = 0; i < myToDos.length; i++) {
   const paragraph = document.createElement('p');
   paragraph.textContent = myToDos[i].text;
   document.querySelector('main').appendChild(paragraph);
 };
+*/
+
+
+
+// todos left
+/*
+let toDoLeft = myToDos.filter(function (todo) {
+  return todo.completion === 'no';
+});
 
 const todoLeftTitle = document.createElement('h1');
 todoLeftTitle.textContent = 'Left to do';
@@ -36,8 +70,13 @@ for(let j = 0; j < toDoLeft.length; j++) {
   document.querySelector('main').appendChild(paragraphLeft);
 };
 
+
+
+
+// button
+
 document.querySelector('.myBtn').addEventListener('click', function (event) {
   console.log('Click was done.')
 });
 
- 
+*/
