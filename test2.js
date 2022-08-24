2b9105bc11f90771251f1ccb1f3a6bb67d4a3b2b
Richard Škoda <richard.skoda72@gmail.com>
2022-08-24 21:59:09 +0200

    Checkbox excercise. Not complete yet.

--- test2.js
+++ test2.js
@@ -1,11 +1,39 @@
-const body = document.querySelector('body');
 const form = document.querySelector('#my-form');
+const alertText = document.querySelector('#alert');
 
-form.addEventListener('submit', function (event) {
+const tasksDiv = document.createElement('div');
+document.body.appendChild(tasksDiv);
+
+
+form.addEventListener('submit', function(event) {
     event.preventDefault();
-    let color = event.target.elements.color.value;
+    const getInput = event.target.elements.task.value;
 
-    body.style.backgroundColor = color;
+    if(getInput === '') {
+        alertText.style.display = 'inline-block';
+    } else {
+        alertText.style.display = 'none';
 
-    event.target.elements.color.value = '';
-})
+        const id = '' + new Date().getTime();
+
+        const oneTaskDiv = document.createElement('div');
+        oneTaskDiv.style.display = 'flex'
+        tasksDiv.appendChild(oneTaskDiv);
+
+        const newTask = document.createElement('label');
+        newTask.style.marginLeft = '20px';
+        newTask.htmlFor = `check-task${id}`;
+        newTask.textContent = getInput;
+
+        const checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.style.marginLeft = '20px';
+        checkBox.id = `check-task${id}`;
+
+        oneTaskDiv.appendChild(checkBox)
+        oneTaskDiv.appendChild(newTask)
+
+        event.target.elements.task.value = ''
+        }
+
+});
